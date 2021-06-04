f07dd698aaa4f6d1e5b2452d363f28587c48dd4b
Victor Akio <victorakioz@gmail.com>
2021-06-04 18:41:08 -0300

    Include tests to application

---
+++ src/components/Async/Async.spec.tsx
@@ -0,0 +1,18 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { Async } from ".";
+
+test("it renders correctly", async () => {
+  render(<Async />);
+
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(screen.queryByText("Button"));
+  // await waitFor(() => {
+  //   return expect(screen.getByText("Button")).not.toBeInTheDocument();
+  // });
+});
